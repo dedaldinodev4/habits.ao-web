8e41089c7798e0e0d1a164d6a0f4d966c37ed4e8
Dedaldino Daniel <dedaldinodev4@gmail.com>
2025-01-17 20:37:17 +0100

    to be continue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,32 +14,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [loading, setLoading] = useState<boolean>(true);
   const { user } = useContext(AuthContext);
   const pathname = usePathname()
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000); 
+    setTimeout(() => setLoading(false), 1000);
   }, []);
 
-  if(!user && !isUserLogged(pathname)) {
+  if (!user && !isUserLogged(pathname)) {
     redirect('/auth/signin');
   }
 
-  if(user && isUserLogged(pathname)) {
+  if (user && isUserLogged(pathname)) {
     redirect('/');
   }
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <AuthProvider>
-          <div className="dark:bg-boxdark-2 dark:text-bodydark">
-            {loading ? <Loader /> : children}
-          </div>
-        </AuthProvider>   
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? <Loader /> : children}
+        </div>
       </body>
     </html>
   );
